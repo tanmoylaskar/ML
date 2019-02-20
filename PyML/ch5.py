2733695d3a1e3d46b3c5d4ae8eef1c9e5475071a
Tanmoy Laskar <tanmoylaskar@gmail.com>
2019-02-20 17:01:35 +0000

    Feb 20

--- PyML/ch5.py
+++ PyML/ch5.py
@@ -10,3 +10,57 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train_std = sc.fit_transform(X_train)
 X_test_std  = sc.transform(X_test)
+
+import numpy as np
+cov_mat = np.cov(X_train_std.T)
+eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
+
+
+tot = sum(eigen_vals)
+var_exp = [(i / tot) for i in sorted(eigen_vals, reverse=True)]
+cum_var_exp = np.cumsum(var_exp)
+import matplotlib.pyplot as plt
+plt.bar(range(1,14), var_exp, alpha=0.5, align='center', label='individual explained variance')
+plt.step(range(1,14), cum_var_exp, where='mid', label='cumulative explained variance')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal component index')
+plt.legend(loc='best')
+plt.show()
+
+# Make a list of (eigenvalue, eigenvector) tuples
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i]) for i in range(len(eigen_vals))]
+# Sort the (eigenvalue, eigenvector) tuples from high to low
+eigen_pairs.sort(key=lambda k: k[0], reverse=True)
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis], eigen_pairs[1][1][:, np.newaxis]))
+
+X_train_std[0].dot(w)
+X_train_pca = X_train_std.dot(w)
+
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_pca[y_train==l, 0], X_train_pca[y_train==l, 1], c=c, label=l, marker=m) 
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.show()
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+lr = LogisticRegression()
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+lr.fit(X_train_pca, y_train)
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.show()
+
+
+plot_decision_regions(X_test_pca, y_test, classifier=lr)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(loc='lower left')
+plt.show()
