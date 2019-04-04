32b187dffa75eb06814cddc051001d3bda6a84fb
Tanmoy Laskar <tanmoylaskar@gmail.com>
2019-04-04 18:49:41 +0100

    2018 April 4

--- PyML/ch6.py
+++ PyML/ch6.py
@@ -1,6 +1,6 @@
 # Making pipelines
 import pandas as pd
-df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data', header=None)
+df = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data', header=None)
 from sklearn.preprocessing import LabelEncoder
 
 
@@ -120,3 +120,81 @@ print('Test accuracy: %.3f' % clf.score(X_test, y_test))
 gs = GridSearchCV(estimator=pipe_svc, param_grid=param_grid, scoring='accuracy', cv=2)
 scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=5)
 print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+# 2019 April 4
+# Confusion matrix
+from sklearn.metrics import confusion_matrix
+pipe_svc.fit(X_train, y_train)
+y_pred = pipe_svc.predict(X_test)
+confmat = confusion_matrix(y_true=y_test, y_pred=y_pred)
+print(confmat)
+
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score, f1_score
+print('Precision: %.3f' % precision_score(y_true=y_test, y_pred=y_pred))
+print('Recall: %.3f' % recall_score(y_true=y_test, y_pred=y_pred))
+print('F1: %.3f' % f1_score(y_true=y_test, y_pred=y_pred))
+
+from sklearn.metrics import make_scorer, f1_score
+scorer = make_scorer(f1_score, pos_label=0)
+gs = GridSearchCV(estimator=pipe_svc, param_grid=param_grid, scoring=scorer, cv=10)
+gs = gs.fit(X_train, y_train)
+print(gs.best_score_)
+
+
+# ROC curve
+from sklearn.metrics import roc_curve, auc
+from scipy import interp
+
+pipe_lr = make_pipeline(StandardScaler(),
+                        PCA(n_components=2),
+                        LogisticRegression(penalty='l2',
+                                           random_state=1,
+                                           C=100.0))
+
+X_train2 = X_train[:, [4, 14]]
+
+cv = list(StratifiedKFold(n_splits=3,
+                          random_state=1).split(X_train,
+                                                y_train))
+fig = plt.figure(figsize=(7, 5))
+
+mean_tpr = 0.0
+mean_fpr = np.linspace(0, 1, 100)
+all_tpr = []
+
+for i, (train, test) in enumerate(cv):
+    probas = pipe_lr.fit(X_train2[train],
+                 y_train[train]).predict_proba(X_train2[test])
+    fpr, tpr, thresholds = roc_curve(y_train[test],
+                                     probas[:, 1],
+                                     pos_label=1)
+    mean_tpr += interp(mean_fpr, fpr, tpr)
+    mean_tpr[0] = 0.0
+    roc_auc = auc(fpr, tpr)
+    plt.plot(fpr,
+             tpr,
+             label='ROC fold %d (area = %0.2f)'
+                   % (i+1, roc_auc))
+plt.plot([0, 1],
+         [0, 1],
+         linestyle='--',
+         color=(0.6, 0.6, 0.6),
+         label='random guessing')
+
+mean_tpr /= len(cv)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+plt.plot(mean_fpr, mean_tpr, 'k--',
+         label='mean ROC (area = %0.2f)' % mean_auc, lw=2)
+plt.plot([0, 0, 1],
+         [0, 1, 1],
+         linestyle=':',
+         color='black',
+         label='perfect performance')
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('false positive rate')
+plt.ylabel('true positive rate')
+plt.legend(loc="lower right")
+plt.show()
